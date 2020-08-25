45a0a264f69dfebd4ebb6a91da819744b0353dd8
Shreiber <ss@sem-wolf.org>
2020-08-25 16:34:24 -0700

    phase3, 4: share ptr class

--- shared_ptr.h
+++ shared_ptr.h
@@ -1,14 +1,34 @@
-//
-// Created by a on 8/25/20.
-//
+
 
 #ifndef SMARTPOINTERS_SHARED_PTR_H
 #define SMARTPOINTERS_SHARED_PTR_H
 
 
-class shared_ptr {
+#include <iostream>
 
+template <class T>
+class SharedPtr {
+public:
+    explicit SharedPtr(T* ptr = NULL);
+    ~SharedPtr();
+    SharedPtr(SharedPtr& other);
+
+    T* get_ptr() const;
+
+    SharedPtr& operator=(SharedPtr& other);
+    explicit operator bool() const;
+
+    T operator*()const ;
+    T* operator->()const ;
+
+private:
+    T* m_ptr;
+    size_t* m_counter;
+
+    void delete_ptr();
+
+    friend class TestSharedPtr;
 };
 
-
+#include "shared_ptr.hpp"
 #endif //SMARTPOINTERS_SHARED_PTR_H
