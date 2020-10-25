8857fdc14fbc22eb346887d18a16283e5247dce9
Shreiber <ss@sem-wolf.org>
2020-10-24 22:02:34 -0700

    add main file

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "test/test.h"
+
+
+int main() {
+
+    TestSafePtr testSafePtr;
+    testSafePtr.test_copy_ctor();
+    testSafePtr.test_assigment_operator();
+    testSafePtr.test_operator_asterisk();
+    testSafePtr.test_operator_arrow();
+
+    TestUniquePtr testUniquePtr;
+    testUniquePtr.test_copy_ctor();
+    testUniquePtr.test_assigment_operator();
+    testUniquePtr.test_operator_asterisk();
+    testUniquePtr.test_operator_arrow();
+
+    TestSharedPtr testSharedPtr;
+    testSharedPtr.test_copy_ctor();
+    testSharedPtr.test_assigment_operator();
+    testSharedPtr.test_operator_asterisk();
+    testSharedPtr.test_operator_arrow();
+
+    return 0;
+}
